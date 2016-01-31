d8764dcdb830311fbdcbd5e282bc749321f301b6
Suhas Rao <susudhee@microsoft.com>
2016-01-30 16:30:40 -0800

    Permute

---
+++ DSAlgos/DSAlgos/Puzzles/Anagrams.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgos.Puzzles
+{
+    /// <summary>
+    /// Given a string s generates all permutations - permutations of abc = abc, acb, bac, bca, cab, cba
+    /// </summary>
+    public class Anagrams
+    {
+        public void Run()
+        {
+            Permute("abcd");
+        }
+
+        string Permute(string s)
+        {
+            foreach(var charcter in s.ToCharArray())
+            {
+                return charcter + Permute(s.Replace(charcter.ToString(), ""));
+            }
+
+            return "";
+        }
+
+    }
+}
