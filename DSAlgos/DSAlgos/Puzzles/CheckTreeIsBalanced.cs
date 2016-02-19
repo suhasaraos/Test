00092c17fc96e4b9fe3d0559c0776de374f789f5
Suhas Rao <susudhee@microsoft.com>
2016-02-19 08:33:24 -0800

    Somemissing files

---
+++ DSAlgos/DSAlgos/Puzzles/CheckTreeIsBalanced.cs
@@ -0,0 +1,73 @@
+﻿using DSAlgos.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgos.Puzzles
+{
+    public class CheckTreeIsBalanced
+    {
+
+        /// <summary>
+        /// To check if give tree is balanced
+        /// </summary>
+        public void Run()
+        {
+
+            BinarySearchTree<int> newTreeUnbalanced = new BinarySearchTree<int>();
+            newTreeUnbalanced.Add(7);
+            newTreeUnbalanced.Add(8);
+            newTreeUnbalanced.Add(5);
+            newTreeUnbalanced.Add(6);
+            newTreeUnbalanced.Add(14);
+            newTreeUnbalanced.Add(11);
+            newTreeUnbalanced.Add(9);
+            newTreeUnbalanced.Add(12);
+            newTreeUnbalanced.Add(10);
+            newTreeUnbalanced.Add(17);
+              //             7
+              //        5         8
+              //      x   6      x   14
+              //         x  x      11  17
+              //                9    12
+              //               x  10
+
+            VisitNode(newTreeUnbalanced.Root);
+        }
+
+        ////Algo
+        //      5
+        //   3     7
+        // 1   4  6  8
+
+        int VisitNode(BinaryTreeNode<int> treeNode)
+        {
+            int leftVal = 0, rightVal = 0;
+            if(treeNode.Left == null)
+            {
+                leftVal = -1;
+            }
+            else
+            {
+                leftVal = VisitNode(treeNode.Left);
+            }
+            if(treeNode.Right == null)
+            {
+                rightVal = -1;
+            }
+            else
+            {
+                rightVal = VisitNode(treeNode.Right);
+            }
+
+            if(Math.Abs(leftVal - rightVal) > 1)
+            {
+                Console.WriteLine("Tree is not balanced");
+            }
+
+            return (leftVal > rightVal ? leftVal : rightVal) + 1;
+        }
+     }
+}
---
+++ DSAlgos/DSAlgos/Puzzles/CreateBSTUsingSortedArray.cs
@@ -0,0 +1,57 @@
+﻿using DSAlgos.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSAlgos.Puzzles
+{
+    /// <summary>
+    ///   //Given a sorted array with unique int, write algo to create binary search tree with optimal height.
+    ///EX: { 2,3,4,5,6,7,8 }
+    /// </summary>
+    public class CreateBSTUsingSortedArray
+    {
+        //Ans
+        //         5
+        //       3    7
+        //      2 4  6 8
+
+        //Find ele at length/2 (odd) and insert , call recursively left arry and right arr.
+        // if length is 1 - insert, if lngth
+
+        public void Run()
+        {
+            int[] inputArr = new[] { 2, 3, 4, 6, 7, 8 };
+            var newTree = new BinarySearchTree<int>();
+
+            InsertToTree(inputArr, newTree);                  
+        }
+
+        void InsertToTree(int[] arr, BinarySearchTree<int> tree)
+        {
+            if (arr.Length <= 0)
+                return;
+
+            int midLoc = arr.Length / 2;
+            tree.Add(arr[midLoc]);
+            if (midLoc == 0)
+                return;
+
+            int[] leftArray = new int[midLoc];
+            for (int i=0; i< midLoc; i++)
+            {
+                leftArray[i] = arr[i];
+            }
+            InsertToTree(leftArray, tree);
+
+            int[] rightArray = new int[arr.Length - (midLoc + 1)];
+            for (int i=midLoc + 1; i<arr.Length; i++)
+            {
+                rightArray[i - (midLoc + 1)] = arr[i];
+            }
+            InsertToTree(rightArray, tree);
+        }
+    }
+}
